5a82dc9ba8c25acc33d7c4d6129153d091b6f50b
vanshika777-hub <124432309+vanshika777-hub@users.noreply.github.com>
2023-02-04 17:07:24 +0530

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,6 @@
+#Taking user input
+name= input("whats your name?")
+#Converting user input into lower case nad avoid whitespace
+name = name.strip().lower()
+#printing desired output
+print(f"hello,{name}")
